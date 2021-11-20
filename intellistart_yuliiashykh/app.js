a67f50a4000c639be904e47915c0376915fe4437
ysh-intelli <94750188+ysh-intelli@users.noreply.github.com>
2021-11-20 17:03:49 +0200

    Add files via upload

---
+++ intellistart_yuliiashykh/app.js
@@ -0,0 +1,5 @@
+document.getElementById("credits__btn").addEventListener("click", myFunction);
+
+function myFunction() {
+document.getElementById("pexels-cat").innerHTML = "Let's go! <br> Get in touch: yuliia.shykh@gmail.com"
+}
